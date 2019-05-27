4891cb7623c73881be378c61528570ee240237d6
sonnlh <sonnlh57@gmail.com>
2019-05-27 09:27:00 +0700

    updat sidbar

--- Code/Front-End/mind-sending/src/components/Home.js
+++ Code/Front-End/mind-sending/src/components/Home.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import Menu from './Menu';
+import SideBar from './SideBar';
 class Home extends Component {
+
+	
   render(){
      return (
     <div className="App">
       <Menu />
+	  <SideBar className="sidebar mt60" />
     </div>
   );
   }
---
+++ Code/Front-End/mind-sending/src/components/SideBar.js
@@ -0,0 +1,89 @@
+import React, {Component} from 'react';
+class Sidebar extends Component {
+constructor (props) {
+  super(props);
+    this.state = {
+     condition: true
+    };
+  }
+
+
+  render(){
+     return (
+    <div className="wrapper">
+    
+    <nav  className= { this.state.condition ? "sidebar active" : "sidebar" }>
+        <div className="sidebar-header">
+            <h3>SideBar</h3>
+        </div>
+
+        <ul className="list-unstyled components">
+            <p>Heading</p>
+            <li className="active">
+                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Home</a>
+                <ul className="collapse list-unstyled" id="homeSubmenu">
+                    <li>
+                        <a href="#">Home 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Home 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Home 3</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">About</a>
+            </li>
+            <li>
+                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Pages</a>
+                <ul className="collapse list-unstyled" id="pageSubmenu">
+                    <li>
+                        <a href="#">Page 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Page 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Page 3</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Portfolio</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </nav>
+    <div id="content">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+
+            <button type="button" id="sidebarCollapse" onClick={this.openSideBar} className="btn btn-info">
+                <i className="fas fa-align-left"></i>
+                <span>Toggle Sidebar</span>
+            </button>
+
+        </div>
+    </nav>
+</div>
+
+</div>
+  );
+  }
+
+   openSideBar = () =>{
+   
+   this.setState({
+      condition: !this.state.condition
+    });
+    
+   }
+}
+
+
+
+export default Sidebar;
