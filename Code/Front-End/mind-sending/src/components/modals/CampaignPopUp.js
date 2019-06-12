ad4c4f84b5cdfbcb78bd35433a9f245aef83978f
sonnlh <sonnlh57@gmail.com>
2019-06-13 00:22:18 +0700

    campaign

--- Code/Front-End/mind-sending/src/components/modals/CampaignPopUp.js
+++ Code/Front-End/mind-sending/src/components/modals/CampaignPopUp.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import CreateCampaign from './../../pages/DashboardPage/CampaignsComponent/CreateCampaigns';
+import {connect} from 'react-redux';
+import * as actions from './../../actions/index';
 
 class CampaignPopUp extends Component {
+  
    constructor(props) {
      super(props);
 
      this.state = {
+       campaignName: "",
        visible: true,
        dropdown_visible: false,
      };
@@ -16,10 +21,18 @@ class CampaignPopUp extends Component {
      })
    }
 
-
+   onChange = (event) => {
+    this.setState({campaignName: event.target.value});
+    console.log(this.state.campaignName);
+  }
+  onCreateName=()=>{
+    this.props.onCreateCampaign(this.state.campaignName);
+  }
   
   render(){
+    var campaign = this.state.campaignName
      return (
+       
          <div className="popup" id="popup">
     <div className="popup-inner">
       <div className="popupphoto">
@@ -29,23 +42,41 @@ class CampaignPopUp extends Component {
 
         <div className="popuptext">
         <div className="popup-header">
-        <img className="img-pop-up" src="https://img.icons8.com/cool-color/64/000000/upload-mail.png"/>
+        {/* <img className="img-pop-up" src="https://img.icons8.com/cool-color/64/000000/upload-mail.png"/> */}
         <h1>CAMPAIGN NAME</h1>
         </div>
         <p>Keep your subscribers engaged by sharing your latest news, promoting a line of products, or announcing an event.</p>
-        <input className="iput_pop_up" placeholder="Write down your campaign name" autocomplete="off"/> 
+        <input onChange={this.onChange.bind(this)} className="iput_pop_up" placeholder="Write down your campaign name" autocomplete="off"/> 
         <div>
-        <btn className="btn-create-segment-pop-up" href="#">Begin</btn>
-        <btn className="btn-create-segment-pop-up" href="#">Cancel</btn>
+        <Link  className="btn-create-segment-pop-up" 
+        onClick={this.onCreateName}
+        to={{
+          pathname: '/create-campaign',
+          
+          }}>Begin</Link>
+        <a className="btn-create-segment-pop-up" href="#">Cancel</a>
         </div>
       
         
       </div>
       <a class="closepopup" href="#">X</a>
     </div>
+    
     </div>
       );
   }
 
 }
-export default CampaignPopUp;
+const mapStateToProps = (state) => {
+  return {
+   newCampaign : state.campaignName
+  }
+  };
+  const mapDispatchToProps = (dispatch, props) => {
+    return{
+      onCreateCampaign: (newCampaign)=> {
+        dispatch(actions.createNewCampaign(newCampaign));
+      }
+    };
+  };
+export default connect(mapStateToProps, mapDispatchToProps) (CampaignPopUp);
