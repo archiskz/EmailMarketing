36d25963af8c20944277a42b60b7c61c2d7f99b7
sonnlh <sonnlh57@gmail.com>
2019-06-22 15:50:14 -0700

    haha

--- Code/emailmarketing/emailmarketing/src/main/java/com/emailmkt/emailmarketing/Config/WebSecurity.java
+++ Code/emailmarketing/emailmarketing/src/main/java/com/emailmkt/emailmarketing/Config/WebSecurity.java
@@ -90,7 +90,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                         "/webjars/**",
                         "favicon.ico"
                 ).permitAll()
-                .anyRequest().authenticated()
+//                .anyRequest().authenticated()
                 .and()
                 .addFilter(authenticationFilter)
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()))
