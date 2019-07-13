5e5c0d2ea50fb8f53a91f814bda26ccd2d407225
Tyler Nguyen <43690659+TylerNguyen62447@users.noreply.github.com>
2019-07-13 13:32:43 +0700

    UI

--- Code/Front-End/mind-sending/src/pages/DashboardPage/EmbededFormComponent/EmbededForm.js
+++ Code/Front-End/mind-sending/src/pages/DashboardPage/EmbededFormComponent/EmbededForm.js
@@ -34,6 +34,7 @@ class EmbededForm extends Component {
   <label for="mce-EMAIL">Email Address  <span className="asterisk">*</span>
 </label>
   <input type="email" value="" name="EMAIL" className="required email" id="mce-EMAIL"/>
+  <div for="mce-EMAIL" class="mce_inline_error">This field is required.</div>
 </div>
 <div className="mc-field-group">
   <label for="mce-FNAME">First Name </label>
@@ -53,7 +54,7 @@ class EmbededForm extends Component {
          </div>
          <div className="section_embed">
          <h3>Copy/paste onto your site</h3>
-         <form className="relative_embed">
+         <form className="embed_display_form">
                     <textarea name="content" class="full-width-embed" Style="height:200px" id="embed-form-code" onclick="mojo.utils.selectAll('embed-form-code')">
 
                     </textarea>
