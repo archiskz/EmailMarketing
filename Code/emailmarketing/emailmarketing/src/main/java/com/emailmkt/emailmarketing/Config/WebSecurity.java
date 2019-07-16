b0b178dbe73b56ac5360f8fd331e023a3cdc1d35
archiskz <tannmse625032@fpt.edu.vn>
2019-07-17 03:21:03 +0700

    z
    Commit

--- Code/emailmarketing/emailmarketing/src/main/java/com/emailmkt/emailmarketing/Config/WebSecurity.java
+++ Code/emailmarketing/emailmarketing/src/main/java/com/emailmkt/emailmarketing/Config/WebSecurity.java
@@ -21,7 +21,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
-import org.springframework.security.web.session.SessionManagementFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
