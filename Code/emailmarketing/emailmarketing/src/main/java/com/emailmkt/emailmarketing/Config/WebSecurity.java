c2d8b3075be9e10b5ef8dfe2ada9f93b7bcdd98c
archiskz <tannmse625032@fpt.edu.vn>
2019-07-16 22:35:03 +0700

    cors

--- Code/emailmarketing/emailmarketing/src/main/java/com/emailmkt/emailmarketing/Config/WebSecurity.java
+++ Code/emailmarketing/emailmarketing/src/main/java/com/emailmkt/emailmarketing/Config/WebSecurity.java
@@ -8,6 +8,7 @@ package com.emailmkt.emailmarketing.Config;
 import com.emailmkt.emailmarketing.impl.UserDetailsServiceImpl;
 import com.emailmkt.emailmarketing.security.JWTAuthenticationFilter;
 import com.emailmkt.emailmarketing.security.JWTAuthorizationFilter;
+import com.google.common.collect.ImmutableList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -20,6 +21,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.security.web.session.SessionManagementFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static com.emailmkt.emailmarketing.constants.SecurityConstant.SIGN_UP_URL;
 
@@ -53,7 +60,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         JWTAuthenticationFilter authenticationFilter = new JWTAuthenticationFilter(authenticationManager());
-        authenticationFilter.setFilterProcessesUrl("/login");
+
+        authenticationFilter.setFilterProcessesUrl("/api/login");
 //
 //        http.cors().and().csrf().disable().authorizeRequests()
 //
@@ -76,9 +84,11 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 //                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
 //                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 //        ;
-        http
+//        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+//                .and().csrf().disable()
 
-                .authorizeRequests()
+        http.cors().and().csrf().disable().authorizeRequests()
+
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
 
                 .antMatchers(
@@ -89,13 +99,35 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                         "/webjars/**",
                         "favicon.ico"
                 ).permitAll()
-//                .anyRequest().authenticated()
+                .anyRequest().authenticated()
                 .and()
+
                 .addFilter(authenticationFilter)
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()))
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 // it's indicate all request will be secure
-        http.csrf().disable();
+
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(ImmutableList.of("*"));
+        configuration.setAllowedMethods(ImmutableList.of("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+//        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowedHeaders(Arrays.asList("X-Requested-With","Origin","Content-Type","Accept","Authorization","Content-Type"));
+        configuration.setExposedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Authorization, x-xsrf-token, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, " +
+                "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"));
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 
