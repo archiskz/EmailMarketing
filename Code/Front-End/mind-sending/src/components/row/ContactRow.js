5eeed6bc1434a508c42130d3dffc326fa019146d
sonnlh <sonnlh57@gmail.com>
2019-08-01 16:34:55 +0700

    a

--- Code/Front-End/mind-sending/src/components/row/ContactRow.js
+++ Code/Front-End/mind-sending/src/components/row/ContactRow.js
@@ -65,7 +65,7 @@ class ContactRow extends Component {
           <td class="md_tablet6_tbody_td">
           {this.props.contactActions}
           <a class="fas fa-edit margin_td_fontawsome" onClick={()=>this.openModal()} title="Edit"> </a>
-          <a class="fas fa-trash-alt" onClick={()=>this.openModalDelete()} title="Delete"> </a>
+          {/* <a class="fas fa-trash-alt" onClick={()=>this.openModalDelete()} title="Delete"> </a> */}
           </td>
           <Modal style={{"paddingLeft": "10px","paddingRight": "10px"}} visible={this.state.updateContactVisible} width="440" height="660" effect="fadeInUp" onClickAway={() => this.closeModal()}>
                 <form class="contact1-form validate-form">
