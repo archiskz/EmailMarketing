445b9a395644eadc5fc2138490fcb543e3fbdff9
archiskz <50858519+archiskz@users.noreply.github.com>
2019-09-24 15:59:24 +0700

    v

--- Code/emailmarketing/emailmarketing/src/main/java/com/emailmkt/emailmarketing/repository/CampaignRepository.java
+++ Code/emailmarketing/emailmarketing/src/main/java/com/emailmkt/emailmarketing/repository/CampaignRepository.java
@@ -8,24 +8,27 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
-public interface CampaignRepository extends JpaRepository<Campaign,Integer> {
-        Campaign findByNameAndAccount_id(String name, int account_id);
-//        Campaign findById(int id);
-@Transactional        Campaign findCampaignById(int id);
-        List<Campaign> findAllByAutomationIsFalseOrderByCreatedTimeDesc();
+public interface CampaignRepository extends JpaRepository<Campaign, Integer> {
+    Campaign findByNameAndAccount_id(String name, int account_id);
 
-        List<Campaign> findCampaignByAccount_idAndAutomationIsFalseOrderByCreatedTimeDesc(int account_id);
 
-        Campaign findTopByAccount_idAndAutomationIsFalseAndStatusContainsOrderByCreatedTimeDesc(int accountId,String status);
+    @Transactional
+    Campaign findCampaignById(int id);
 
-        List<Campaign> findTop5ByOrderByCreatedTimeDesc();
+    List<Campaign> findAllByAutomationIsFalseOrderByCreatedTimeDesc();
+
+    List<Campaign> findCampaignByAccount_idAndAutomationIsFalseOrderByCreatedTimeDesc(int account_id);
+
+    Campaign findTopByAccount_idAndAutomationIsFalseAndStatusContainsOrderByCreatedTimeDesc(int accountId, String status);
+
+    List<Campaign> findTop5ByOrderByCreatedTimeDesc();
 
 
 //        Campaign findTop1ByOrderByCreatedTimeDesc();
 
-        List<Campaign> findAllByOrderByCreatedTimeDesc();
+    List<Campaign> findAllByOrderByCreatedTimeDesc();
 
-        List<Campaign> findCampaignByAccount_idOrderByCreatedTimeDesc(int account_id);
+    List<Campaign> findCampaignByAccount_idOrderByCreatedTimeDesc(int account_id);
 
 
 }
